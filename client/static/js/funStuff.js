0b527939b758d8e898308277436aaacef1efe53b
Mathew Cohen <mathewjcohen@gmail.com>
2019-07-07 22:07:37 -0700

    single page layout - update

---
+++ client/static/js/funStuff.js
@@ -0,0 +1,24 @@
+const state = {
+  // modalVisible: 0,
+  // modalEl: document.getElementById("modalBG"),
+};
+
+// modal logic
+// document.getElementById("customForm").addEventListener("click", toggleModal);
+// document.getElementById("closeModal").addEventListener("click", toggleModal);
+
+// function toggleModal() {
+//   if (state.modalVisible === 0) {
+//     state.modalVisible = 1;
+//     state.modalEl.style.top = `${window.scrollY}px`;
+//     state.modalEl.style.visibility = "visible";
+//   } else {
+//     state.modalVisible = 0;
+//     state.modalEl.style.visibility = "hidden";
+//   }
+// }
+
+// TODO: add custom build form and validate
+// function validateForm() {}
+
+// scroll to top
