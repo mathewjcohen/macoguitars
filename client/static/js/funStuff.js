b86224666aa2b4edcefdb8ca06faeb73a6f12689
Mathew Cohen <mathewjcohen@gmail.com>
2019-07-17 22:06:11 -0700

    added smooth scroll

--- client/static/js/funStuff.js
+++ client/static/js/funStuff.js
@@ -1,7 +1,7 @@
-const state = {
-  // modalVisible: 0,
-  // modalEl: document.getElementById("modalBG"),
-};
+// const state = {
+//   // modalVisible: 0,
+//   // modalEl: document.getElementById("modalBG"),
+// };
 
 // modal logic
 // document.getElementById("customForm").addEventListener("click", toggleModal);
@@ -21,4 +21,15 @@ const state = {
 // TODO: add custom build form and validate
 // function validateForm() {}
 
-// scroll to top
+var nav = document.getElementsByTagName("nav")[0];
+var about = document.getElementById("about");
+var photos = document.getElementById("photos");
+var custom = document.getElementById("custom");
+
+function scrollToEl(el) {
+  window.scrollTo({
+    top: el.offsetTop - nav.offsetHeight,
+    left: 0,
+    behavior: "smooth"
+  });
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const compression = require('compression');
+const express = require("express");
+const path = require("path");
+// const bodyParser = require("body-parser");
+const compression = require("compression");
 
 const app = express();
 
@@ -9,9 +9,13 @@ app.use(compression());
 app.use(express.static(__dirname + "/client/static"));
 
 app.set("views", path.join(__dirname, "./client/views"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-var routes_setter = require('./server/config/routes.js');
+app.get("/client/static/js/funStuff.js", function(req, res) {
+  res.sendFile(path.join(__dirname + "/client/static/js/funStuff.js"));
+});
+
+var routes_setter = require("./server/config/routes.js");
 routes_setter(app);
 
 app.listen(8001);
