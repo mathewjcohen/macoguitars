1c0de5437ba04fcbee8a014d14ae5ca7e54ff53b
Mathew Cohen <mathewjcohen@gmail.com>
2019-08-14 22:27:36 -0700

    added fb share meta data

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,4 @@
 module.exports = app => {
   app.get("/", (req, res) => res.render("index"));
-  app.get("/founders-sale/first-five", (req, res) => res.render("sale"));
+  app.get("/founders-sale", (req, res) => res.render("sale"));
 };
