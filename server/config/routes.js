95f98fe104f808e50bf7c05b7227b82cb4a4726c
Mathew Cohen <mathewjcohen@gmail.com>
2021-03-14 22:12:17 -0700

    add tools page placeholder; smooth scroll

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,5 @@
 module.exports = app => {
   app.get("/", (req, res) => res.render("index"));
   app.get("/founders-sale", (req, res) => res.render("sale"));
+  app.get("/tools", (req, res) => res.render("tools"));
 };
