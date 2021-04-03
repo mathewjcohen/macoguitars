f0cb10b4f602ae0653d920c3944d0180d4d42e69
Mathew Cohen <mathewjcohen@gmail.com>
2021-04-02 21:36:26 -0700

    updated some stuff

--- client/static/js/funStuff.js
+++ client/static/js/funStuff.js
@@ -1,9 +1,28 @@
+const header = document.getElementById("header");
+const about = document.getElementById("about");
+const nav = document.getElementsByTagName("nav")[0];
+const photos = document.getElementById("photos");
+const custom = document.getElementById("custom");
+
 const state = {
   modalVisible: 0,
   modalEl: document.getElementById("modalBG"),
   modalImg: document.getElementById("modalImg")
 };
 
+(function() {
+  const currentYear = new Date().getFullYear();
+  const copyEl = document.getElementById("dateCopyright");
+  copyEl.innerHTML = `&copy; MaCo Guitars, ${currentYear}. All rights reserved.`;
+})();
+
+window.onscroll = function() {
+  if (document.documentElement.scrollTop > header.offsetHeight / 2) {
+    header.style.position = "fixed";
+  } else {
+    header.style.position = "relative";
+  }
+};
 // modal logic
 state.modalEl.addEventListener("click", clearImgInModal);
 
@@ -37,11 +56,6 @@ function clearImgInModal() {
 // TODO: add custom build form and validate
 // function validateForm() {}
 
-var nav = document.getElementsByTagName("nav")[0];
-var about = document.getElementById("about");
-var photos = document.getElementById("photos");
-var custom = document.getElementById("custom");
-
 function scrollToEl(el) {
   window.scrollTo({
     top: el.offsetTop - nav.offsetHeight,
@@ -49,68 +63,3 @@ function scrollToEl(el) {
     behavior: "smooth"
   });
 }
-
-// hero fader
-(function() {
-  if (document.getElementById("hero1")) {
-    const size = () => {
-      if (document.documentElement.clientWidth > 650) {
-        return "large";
-      }
-      return "small";
-    };
-    const hero1 = document.getElementById("hero1");
-    const hero2 = document.getElementById("hero2");
-    const photoArray = [
-      `/images/guitars/${size()}/andromeda/3.jpg`,
-      `/images/guitars/${size()}/hera/2.jpg`,
-      `/images/guitars/${size()}/pegasus/2.jpg`,
-      `/images/guitars/${size()}/casseopeia/7.jpg`,
-      `/images/guitars/${size()}/tele/5.jpg`,
-      `/images/guitars/${size()}/andromeda/6.jpg`,
-      `/images/guitars/${size()}/hera/8.jpg`,
-      `/images/guitars/${size()}/pegasus/7.jpg`,
-      `/images/guitars/${size()}/casseopeia/3.jpg`,
-      `/images/guitars/${size()}/tele/16.jpg`
-    ];
-
-    let photoIdx1 = 2;
-    let photoIdx2 = 3;
-    let hero2Visible = 0;
-
-    function fadeIn(el) {
-      el.classList.add("show");
-      el.classList.remove("hide");
-    }
-
-    function fadeOut(el) {
-      el.classList.add("hide");
-      el.classList.remove("show");
-    }
-
-    setInterval(() => {
-      if (photoIdx1 >= photoArray.length || photoIdx2 >= photoArray.length) {
-        photoIdx1 = 0;
-        photoIdx2 = 1;
-      }
-      if (hero2Visible === 0) {
-        fadeOut(hero1);
-        fadeIn(hero2);
-        setTimeout(() => {
-          hero1.style.backgroundImage = `url('${photoArray[photoIdx1]}')`;
-        }, 1500);
-        hero2Visible = 1;
-        photoIdx1 = photoIdx1 + 2;
-      } else {
-        fadeIn(hero1);
-        fadeOut(hero2);
-        setTimeout(() => {
-          hero2.style.backgroundImage = `url('${photoArray[photoIdx2]}')`;
-        }, 1500);
-        hero2Visible = 0;
-        photoIdx2 = photoIdx2 + 2;
-      }
-      clearTimeout();
-    }, 4500);
-  }
-})();
