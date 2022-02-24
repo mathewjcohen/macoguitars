1c2c0b9da1c95d6675743268bc2b2b7ab2bf738b
Mathew Cohen <mathewjcohen@gmail.com>
2022-02-24 00:26:16 -0600

    changed site to basic static site

--- client/static/js/funStuff.js
+++ client/static/js/funStuff.js
@@ -1,67 +1,8 @@
-const header = document.getElementById("header");
-const about = document.getElementById("about");
-const nav = document.getElementsByTagName("nav")[0];
-const photos = document.getElementById("photos");
-const custom = document.getElementById("custom");
-
-const state = {
-  modalVisible: 0,
-  modalEl: document.getElementById("modalBG"),
-  modalImg: document.getElementById("modalImg")
-};
-
 (function() {
   const currentYear = new Date().getFullYear();
   const copyEl = document.getElementById("dateCopyright");
   copyEl.innerHTML = `&copy; MaCo Guitars, ${currentYear}. All rights reserved.`;
 })();
 
-window.onscroll = function() {
-  if (document.documentElement.scrollTop > header.offsetHeight) {
-    header.style.position = "fixed";
-    about.style.paddingTop = `${header.offsetHeight}px`;
-  } else {
-    header.style.position = "relative";
-    about.style.paddingTop = "0px";
-  }
-};
-// modal logic
-state.modalEl.addEventListener("click", clearImgInModal);
-
-function toggleModal() {
-  if (state.modalVisible === 0) {
-    state.modalVisible = 1;
-    state.modalEl.style.top = `${window.scrollY}px`;
-    state.modalEl.style.visibility = "visible";
-  } else {
-    state.modalVisible = 0;
-    state.modalEl.style.visibility = "hidden";
-  }
-}
-
-function placeImgInModal(imgElement) {
-  if (document.documentElement.clientWidth < 650) {
-    state.modalImg.src = imgElement.src;
-  } else {
-    state.modalImg.src = imgElement.getAttribute("data-img-src");
-  }
-  state.modalImg.alt = imgElement.getAttribute("alt");
-  toggleModal();
-}
-
-function clearImgInModal() {
-  state.modalImg.src = "";
-  state.modalImg.alt = "";
-  toggleModal();
-}
-
 // TODO: add custom build form and validate
 // function validateForm() {}
-
-function scrollToEl(el) {
-  window.scrollTo({
-    top: el.offsetTop - nav.offsetHeight,
-    left: 0,
-    behavior: "smooth"
-  });
-}
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,9 @@
 module.exports = app => {
   app.get("/", (req, res) => res.render("index"));
+  app.get("/guitars", (req, res) => res.render("guitars"));
+  app.get("/guitars/atlas", (req, res) => res.render("atlas"));
+  app.get("/about", (req, res) => res.render("about"));
+  app.get("/for-sale", (req, res) => res.render("for-sale"));
   app.get("/founders-sale", (req, res) => res.render("sale"));
   app.get("/tools", (req, res) => res.render("tools"));
 };
