649f9f32217cfa52e369203cfbefb395d6ca9f56
Mathew Cohen <mathewjcohen@gmail.com>
2022-02-24 22:29:27 -0600

    ready for live

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,7 @@
 module.exports = app => {
   app.get("/", (req, res) => res.render("index"));
-  app.get("/guitars", (req, res) => res.render("guitars"));
+  app.get("/guitars", (req, res) => res.render("index"));
   app.get("/guitars/atlas", (req, res) => res.render("atlas"));
-  app.get("/about", (req, res) => res.render("about"));
   app.get("/for-sale", (req, res) => res.render("for-sale"));
   app.get("/founders-sale", (req, res) => res.render("sale"));
   app.get("/tools", (req, res) => res.render("tools"));
